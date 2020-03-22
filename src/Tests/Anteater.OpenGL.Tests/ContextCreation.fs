0cbe22609a7550ce5ec2021f3544a0a42939cb36
Georg Haaser <krauthaufen@awx.at>
2020-03-22 14:46:09 +0100

    sync

--- src/Tests/Anteater.OpenGL.Tests/ContextCreation.fs
+++ src/Tests/Anteater.OpenGL.Tests/ContextCreation.fs
@@ -8,17 +8,10 @@ open Utilities
 
 [<Tests>]
 let simple = 
-    testList "create device" [
-    
-        testPropertyWithConfig { cfg with maxTest = 3 } "integrated" (fun (f : OpenGLFeatures) ->
-            let d = getDevice { queues = 1; nVidia = false; features = f; debug = false }
-            ()
-        ) 
-        testPropertyWithConfig { cfg with maxTest = 3 } "nvidia" (fun (f : OpenGLFeatures) ->
-            let d = getDevice { queues = 1; nVidia = true; features = f; debug = false }
-            ()
-        ) 
-    ]
+    testPropertyWithConfig { cfg with maxTest = 10 } "create device" (fun (f : OpenGLFeatures) ->
+        let d = getDevice { queues = 1; nVidia = false; features = f; debug = false }
+        ()
+    ) 
 
 
 
